b7306a360bd3adfa6aced6d9e3488d65ca98fe6d
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-07-13 11:37:05 -0300

    Requisito 1 e 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,14 +1,14 @@
 const fetchProducts = async (item) => {
-  if (item === undefined) {
-    throw new Error('You must provide an url');
-  }
   try {
+    if (item === undefined) {
+      throw new Error('You must provide an url');
+    }
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
     const request = await fetch(url);
     const json = await request.json();
     return json;
   } catch (error) {
-    return error.message; 
+    return error;
   }
 };
 
